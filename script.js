9fdeacd128fef7dda0d3047118ae3897cfa2ecef
Blues 🦋 <yanis.anciaux.dwwm48@gmail.com>
2024-12-11 16:48:01 +0100

    uptade yanis

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 
-
 if (localStorage.getItem('darkMode') === 'enabled') {
     document.body.classList.add('dark-mode');
 }
 
-
 darkModeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 
- 
     if (document.body.classList.contains('dark-mode')) {
         localStorage.setItem('darkMode', 'enabled');
     } else {
@@ -18,31 +15,24 @@ darkModeToggle.addEventListener('click', () => {
     }
 });
 
-
-
-
 const form = document.getElementById('contactForm');
 const nom = document.getElementById('nom');
 const prenom = document.getElementById('prenom');
 const email = document.getElementById('email');
 const telephone = document.getElementById('telephone');
 
-
 const nomError = document.getElementById('nomError');
 const prenomError = document.getElementById('prenomError');
 const emailError = document.getElementById('emailError');
 const telephoneError = document.getElementById('telephoneError');
 
-
 function validateForm(event) {
     let isValid = true;
-
 
     nomError.style.display = 'none';
     prenomError.style.display = 'none';
     emailError.style.display = 'none';
     telephoneError.style.display = 'none';
-
 
     if (nom.value.trim() === '') {
         nomError.textContent = "Le nom est requis.";
@@ -56,14 +46,12 @@ function validateForm(event) {
         isValid = false;
     }
 
-
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     if (!emailPattern.test(email.value.trim())) {
         emailError.textContent = "Veuillez entrer un email valide.";
         emailError.style.display = 'block';
         isValid = false;
     }
-
 
     const telPattern = /^[0-9]{10}$/;
     if (!telPattern.test(telephone.value.trim())) {
@@ -72,11 +60,9 @@ function validateForm(event) {
         isValid = false;
     }
 
-
     if (!isValid) {
         event.preventDefault();
     }
 }
-
 
 form.addEventListener('submit', validateForm);
